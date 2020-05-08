59b8b9a4bbf12be1dd6ebf536817261b49c704a9
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2020-05-08 16:23:44 -0700

    EJS practice

--- ExpressEJS/app.js
+++ ExpressEJS/app.js
@@ -1,17 +1,20 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.listen(3000, function() {
     console.log("Listening on port 3000");
 });
 
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
     let thing = req.params.thing;
-    res.render("love.ejs", {thing: thing});
+    res.render("love", {thing: thing});
 });
 
 app.get("/posts", function(req, res) {
@@ -20,5 +23,5 @@ app.get("/posts", function(req, res) {
         {title: "Sparkle Fairies", author: "Cindy"},
         {title: "Everyone Dies", author: "Damien"}
     ];
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
