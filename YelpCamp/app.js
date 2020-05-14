34eba95fb2cfe8aa637d6038bdbec68aa88b7412
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2020-05-14 10:57:47 -0700

    added database

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,30 +1,60 @@
 let express = require("express");
 let app = express();
 var bodyParser = require("body-parser");
+let mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let campgrounds = [
-    {name: "Mystic Falls", image: "https://images.freeimages.com/images/large-previews/16d/waterfall-1502869.jpg"},
-    {name: "Dark Forest", image: "https://images.freeimages.com/images/large-previews/bf0/forest-1177728.jpg"},
-    {name: "Tranquil Woods", image: "https://images.freeimages.com/images/large-previews/0e6/campsite-1-1058369.jpg"}
-]
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Swamp of Sorrows",
+//         image: "https://images.freeimages.com/images/large-previews/915/camp-fire-1511939.jpg"
+//     }, function(err, campground) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             console.log("Campground created: ");
+//             console.log(campground);
+//         }
+//     });
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function (err, campgrounds) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds", {campgrounds: campgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
     let name = req.body.name;
     let image = req.body.image;
     let newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect("/campgrounds");
+        }
+    })
 });
 
 app.get("/campgrounds/new", function(req, res) {
