fea5854f68ff4d6a72e0c33cab070cb6490f4710
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2020-05-15 10:19:53 -0700

    added description

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,13 +3,14 @@ let app = express();
 var bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 let campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String, 
+    description: String
 });
 
 let Campground = mongoose.model("Campground", campgroundSchema);
@@ -17,7 +18,8 @@ let Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //     {
 //         name: "Swamp of Sorrows",
-//         image: "https://images.freeimages.com/images/large-previews/915/camp-fire-1511939.jpg"
+//         image: "https://images.freeimages.com/images/large-previews/915/camp-fire-1511939.jpg",
+//         description: "This swamp is said to drain the happiness out of people."
 //     }, function(err, campground) {
 //         if (err) {
 //             console.log(err);
@@ -38,7 +40,7 @@ app.get("/campgrounds", function(req, res) {
             console.log(err);
         }
         else {
-            res.render("campgrounds", {campgrounds: campgrounds});
+            res.render("index", {campgrounds: campgrounds});
         }
     });
 });
@@ -46,7 +48,8 @@ app.get("/campgrounds", function(req, res) {
 app.post("/campgrounds", function(req, res) {
     let name = req.body.name;
     let image = req.body.image;
-    let newCampground = {name: name, image: image};
+    let desc = req.body.image;
+    let newCampground = {name: name, image: image, description: desc};
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err) {
             console.log(err);
@@ -59,6 +62,17 @@ app.post("/campgrounds", function(req, res) {
 
 app.get("/campgrounds/new", function(req, res) {
     res.render("new.ejs");
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(3000, function() {
