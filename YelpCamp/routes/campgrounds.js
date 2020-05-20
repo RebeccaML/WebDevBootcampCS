c07a1e0b7ecd804647d10d9672bccab593b26d47
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2020-05-20 16:15:32 -0700

    editing

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -47,6 +47,32 @@ router.get("/:id", function(req, res) {
     });
 });
 
+// Editing
+
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            res.redirect("/campgrounds");
+        }
+        else {
+            res.render("campgrounds/edit", {campground: foundCampground});
+        }
+    })
+});
+
+router.put("/:id", function(req, res) {
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+        if (err) {
+            res.redirect("/");
+        }
+        else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+});
+
+// Middleware
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
