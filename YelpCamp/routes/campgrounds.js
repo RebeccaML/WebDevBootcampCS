b62a476964ee646b3151a0ab14192a7c9fb42c5f
Rebecca Lowe <rebeccam88@hotmail.co.uk>
2020-05-21 09:48:45 -0700

    campground authorization

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -47,20 +47,15 @@ router.get("/:id", function(req, res) {
     });
 });
 
-// Editing
+// Update
 
-router.get("/:id/edit", function(req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
     Campground.findById(req.params.id, function(err, foundCampground) {
-        if (err) {
-            res.redirect("/campgrounds");
-        }
-        else {
-            res.render("campgrounds/edit", {campground: foundCampground});
-        }
-    })
+        res.render("campgrounds/edit", {campground: foundCampground});
+        });
 });
 
-router.put("/:id", function(req, res) {
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
         if (err) {
             res.redirect("/");
@@ -71,9 +66,9 @@ router.put("/:id", function(req, res) {
     })
 });
 
-// Deleting
+// Delete
 
-router.delete("/:id", function(req, res) {
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndRemove(req.params.id, function(err) {
         if (err) {
             res.redirect("/campgrounds");
@@ -92,5 +87,26 @@ function isLoggedIn(req, res, next) {
     }
     res.redirect("/login");
 };
+
+function checkCampgroundOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if (err) {
+                res.redirect("back");
+            }
+            else {
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                }
+                else {
+                    res.redirect("back");
+                }
+            }
+        });
+    }
+    else {
+        res.redirect("back");
+    }
+}
 
 module.exports = router;
